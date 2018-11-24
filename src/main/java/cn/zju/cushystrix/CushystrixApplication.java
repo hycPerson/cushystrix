b27890a482a4105841173414a847e03ced795c7f
huyyy_1 <huyyy_1@163.com>
2018-11-24 08:54:24 +0800

    first commit

---
+++ src/main/java/cn/zju/cushystrix/CushystrixApplication.java
@@ -0,0 +1,12 @@
+package cn.zju.cushystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CushystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CushystrixApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/zju/cushystrix/CushystrixApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.zju.cushystrix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CushystrixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
